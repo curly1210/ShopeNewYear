6186c8f101a5a743d6c80c308926d8c39d02b201
Cuong Pham <1210.curly@gmail.com>
2024-01-22 01:24:50 +0700

    done basic

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper(".banner-slider", {
+  loop:true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper = new Swiper(".product-slider", {
+  loop:true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper = new Swiper(".comment-slider", {
+  loop:true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+
+// var swiper = new Swiper(".category-slider", {
+//   loop:true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+// });
