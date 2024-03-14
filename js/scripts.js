6409837c2d4637bcfaa980c1fc67780f59584983
Cuong Pham <1210.curly@gmail.com>
2024-03-14 11:08:43 +0700

    add slide show make mysefl

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 var swiper = new Swiper(".banner-slider", {
-  loop:true,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
   },
 });
 
 var swiper = new Swiper(".product-slider", {
-  loop:true,
+  loop: true,
   grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
@@ -14,7 +14,7 @@ var swiper = new Swiper(".product-slider", {
 });
 
 var swiper = new Swiper(".comment-slider", {
-  loop:true,
+  loop: true,
   grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
@@ -26,15 +26,51 @@ var swiper = new Swiper(".comment-slider", {
 });
 
 var swiper = new Swiper(".most-product-slider", {
-  loop:true,
+  loop: true,
   grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
   },
 });
 
+// Myself make slide-show
+const track = document.querySelector(".slider-wrapper");
+const slides = Array.from(track.children);
+const nextButton = document.querySelector(".right");
+const prevButton = document.querySelector(".left");
 
+const slideWidth = slides[0].getBoundingClientRect().width;
 
+// set position the slide
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + "px";
+};
+
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+  currentSlide.classList.remove("current-slide");
+  targetSlide.classList.add("current-slide");
+};
+
+// when I click left, move slides to the left
+
+prevButton.addEventListener("click", () => {
+  const currentSlide = track.querySelector(".current-slide");
+  const prevSlide = currentSlide.previousElementSibling;
+
+  moveToSlide(track, currentSlide, prevSlide);
+});
+
+// when I click right, move slides to the right
+nextButton.addEventListener("click", () => {
+  const currentSlide = track.querySelector(".current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+
+  // move the slide
+  moveToSlide(track, currentSlide, nextSlide);
+});
 
 // var swiper = new Swiper(".category-slider", {
 //   loop:true,
